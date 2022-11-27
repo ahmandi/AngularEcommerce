7c057afae390b8a06e0d720583116c04b42c5d3d
Amanda <amandard94@gmail.com>
2022-11-26 22:26:30 -0300

    feat: backend connected to frontend

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/Food';
 
@@ -14,14 +15,17 @@ export class HomeComponent {
     private foodService: FoodService,
     activatedRoute: ActivatedRoute
   ) {
+    let foodsObservable: Observable<Food[]>;
     activatedRoute.params.subscribe((params) => {
       if (params['searchTerm'])
-        this.foods = this.foodService.getAllFoodsBySearchTerm(
+        foodsObservable = this.foodService.getAllFoodsBySearchTerm(
           params['searchTerm']
         );
       else if (params['tag'])
-        this.foods = this.foodService.getallFoodsByTag(params['tag']);
-      else this.foods = foodService.getAll();
+        foodsObservable = this.foodService.getallFoodsByTag(params['tag']);
+      else foodsObservable = foodService.getAll();
+
+      foodsObservable.subscribe((serverFoods) => (this.foods = serverFoods));
     });
   }
 }
