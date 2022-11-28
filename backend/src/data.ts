1d456108c4f56893adbc7ac2a943afe079c92af5
Amanda <amandard94@gmail.com>
2022-11-27 22:08:01 -0300

    feat: login implemented

--- backend/src/data.ts
+++ backend/src/data.ts
@@ -1,3 +1,20 @@
+export const sample_users: any[] = [
+	{
+		name: 'John Doe',
+		email: 'john@gmail.com',
+		password: '12345',
+		address: 'Toronto On',
+		isAdmin: true,
+	},
+	{
+		name: 'Jane Doe',
+		email: 'jane@gmail.com',
+		password: '12345',
+		address: 'Shanghai',
+		isAdmin: false,
+	},
+];
+
 export const sample_foods: any[] = [
 	{
 		id: '1',
