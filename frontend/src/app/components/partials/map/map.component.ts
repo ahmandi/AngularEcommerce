59fc3139efe251cb16748958f5187019207d4644
Amanda <amandard94@gmail.com>
2022-11-30 16:41:28 -0300

    feat: map feature 75% done

--- frontend/src/app/components/partials/map/map.component.ts
+++ frontend/src/app/components/partials/map/map.component.ts
@@ -1,5 +1,18 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { LatLng, LatLngTuple, map, Map, tileLayer } from 'leaflet';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  icon,
+  LatLng,
+  LatLngExpression,
+  LatLngTuple,
+  LeafletMouseEvent,
+  map,
+  Map,
+  marker,
+  Marker,
+  tileLayer,
+} from 'leaflet';
+import { LocationService } from 'src/app/services/location.service';
+import { Order } from 'src/app/shared/models/Order';
 
 @Component({
   selector: 'map',
@@ -7,13 +20,24 @@ import { LatLng, LatLngTuple, map, Map, tileLayer } from 'leaflet';
   styleUrls: ['./map.component.css'],
 })
 export class MapComponent implements OnInit {
+  @Input()
+  order!: Order;
+
+  private readonly MARKER_ZOOM_LEVEL = 16;
+  private readonly MARKER_ICON = icon({
+    iconUrl:
+      'https://res.cloudinary.com/foodmine/image/upload/v1638842791/map/marker_kbua9q.png',
+    iconSize: [42, 42],
+    iconAnchor: [21, 42],
+  });
   private readonly DEFAULT_LATLNG: LatLngTuple = [13.75, 21.62];
 
   @ViewChild('map', { static: true })
   mapRef!: ElementRef;
 
   map!: Map;
-  constructor() {}
+  currentMarker!: Marker;
+  constructor(private locationService: LocationService) {}
 
   ngOnInit(): void {
     this.initializeMap();
@@ -27,5 +51,41 @@ export class MapComponent implements OnInit {
     }).setView(this.DEFAULT_LATLNG, 1);
 
     tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(this.map);
+
+    this.map.on('click', (e: LeafletMouseEvent) => {
+      this.setMarker(e.latlng);
+    });
+  }
+
+  findMyLocation() {
+    this.locationService.getCurrentLocation().subscribe({
+      next: (latlng) => {
+        this.map.setView(latlng, this.MARKER_ZOOM_LEVEL);
+        this.currentMarker.setLatLng(latlng);
+      },
+    });
+  }
+  setMarker(latlng: LatLngExpression) {
+    this.addressLatLng = latlng as LatLng;
+
+    if (this.currentMarker) {
+      this.currentMarker.setLatLng(latlng);
+      return;
+    }
+
+    this.currentMarker = marker(latlng, {
+      draggable: true,
+      icon: this.MARKER_ICON,
+    }).addTo(this.map);
+
+    this.currentMarker.on('dragend', () => {
+      this.addressLatLng = this.currentMarker.getLatLng();
+    });
+  }
+
+  set addressLatLng(latlng: LatLng) {
+    latlng.lat = parseFloat(latlng.lat.toFixed(8));
+    latlng.lng = parseFloat(latlng.lat.toFixed(8));
+    this.order.addressLatLng = latlng;
   }
 }
